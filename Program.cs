d896c166622321706d0ef10619d682b8bf8a53bd
nhtonmoy <38049606+nhtonmoy@users.noreply.github.com>
2020-01-15 18:38:48 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+namespace DotNetCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var reader = new System.IO.StreamReader(@"data__2019-11-28.csv"))
+            {
+                reader.ReadLine();
+                System.Collections.Generic.List<string> listA = new System.Collections.Generic.List<string>();
+                System.Collections.Generic.List<string> listB = new System.Collections.Generic.List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    listA.Add(values[6]);
+                    listB.Add(values[0]);
+                }
+
+                int fCount=0;
+                int mCount=0;
+                int total=0;
+                foreach(var i in listA)
+                {
+                    total++;
+                    if(i.Contains("FEMALE"))
+                    {
+                        fCount++;
+                    }
+                }
+                mCount=total-fCount;
+
+                Console.WriteLine("Female count="+ fCount);
+                Console.WriteLine("Male Count= "+ mCount);
+                List<string> dist = listB.Distinct().ToList();
+
+                int uniqPolicyCount=0;
+                foreach(var i in dist)
+                {
+                    uniqPolicyCount++;
+                }
+
+                Console.WriteLine(uniqPolicyCount);
+            }
+        }
+    }
+}
---
+++ data.py
@@ -0,0 +1,23 @@
+import csv
+with open('data__2019-11-28.csv', newline='') as csvfile:
+    spamreader = csv.DictReader(csvfile)
+    
+    uniq={}
+    i=0
+    mCount=0
+    fCount=0
+    for row in spamreader:
+        policyNumber=row['policyNumber']
+        if policyNumber in uniq:
+            continue
+        uniq[policyNumber]=1
+        i=i+1
+
+        gender=row['gender']
+        if gender=="FEMALE":
+            fCount=fCount+1
+        elif gender=="MALE":
+            mCount=mCount+1
+
+    print("unique policy number: ",i)
+    print("male count: ", mCount, " & female count: ", fCount)
